215c5c20e6dc17503903cf4b8f4fd127340cdee0
Hasan-Mahmood231 <hk4145559@gmail.com>
2025-09-23 16:53:01 +0500

    clean the tables in brozen layer and then insert into silver layer.

--- scripts/bronze/BrozenDatacleaning crm_cust_info table.sql
+++ scripts/bronze/BrozenDatacleaning crm_cust_info table.sql
@@ -16,6 +16,19 @@ where cst_firstname != TRIM(cst_firstname)
 select distinct cst_martial_status
 from brozen.crm_cust_info	
 
+
+--the crm_prd_info table clean and then insert into silver layer.
+
+SELECT*
+FROM silver.crm_prd_info
+
+--the sales_deail table clean and then insert into silver layer.
+
+SELECT
+sls_sales
+FROM silver.crm_sales_details
+WHERE sls_sales <0 OR sls_sales = NULL
+
 --======================================================================sliver layer checking.
 --after cleaning the data check result in silver.
 SELECT
@@ -33,3 +46,4 @@ where cst_firstname != TRIM(cst_firstname)
 
 SELECT*
 FROM silver.crm_cust_info
+--======================================================================
